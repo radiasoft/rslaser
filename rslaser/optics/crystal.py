Fixed population inversion calculation

rslaser/optics/crystal.py
@@ -15,8 +15,6 @@ from scipy.interpolate import RectBivariateSpline, RegularGridInterpolator
 from scipy.interpolate import splrep, splev
 from scipy.optimize import curve_fit
 from scipy.special import gamma
-
-# from scipy.special import exp1
 from rsmath import lct as rslct
 from rslaser.utils.validator import ValidatorBase
 from rslaser.utils import srwl_uti_data as srwutil
@@ -272,7 +270,7 @@ class Crystal(Element):
         for j in range(self.nslice):
             thisSlice = self.slice[j]
             dx = (
-                2.0 * thisSlice.population_inversion.mesh_extent
+                2.0 * thisSlice.population_inversion.mesh_extent * 1.15
             ) / thisSlice.population_inversion.n_cells
             cell_area = dx**2.0 * thisSlice.length
             trans_excited_states += thisSlice.pop_inversion_mesh * cell_area
@@ -384,8 +382,8 @@ class CrystalSlice(Element):
             if "pop_inversion" in k:
                 self.population_inversion[k.replace("pop_inversion_", "")] = params[k]
         x = np.linspace(
-            -self.population_inversion.mesh_extent,
-            self.population_inversion.mesh_extent,
+            -self.population_inversion.mesh_extent * 1.15,
+            self.population_inversion.mesh_extent * 1.15,
             self.population_inversion.n_cells,
         )
         xv, yv = np.meshgrid(x, x)
@@ -396,21 +394,11 @@ class CrystalSlice(Element):
             right=self._right_pump,
         )[self.population_inversion.pump_type](nslice, xv, yv)
 
-        pop_inversion_mesh = np.zeros((len(x), len(x)))
+        excited_states = np.zeros((len(x), len(x)))
         for param_set in param_set_array:
             z = param_set[0]
             slice_front = param_set[1]
             slice_end = param_set[2]
-
-            # calculate correction factor for representing a gaussian pulse with a series of flat-top slices
-            correction_factor = (
-                np.exp(-self.population_inversion.crystal_alpha * slice_front)
-                - np.exp(-self.population_inversion.crystal_alpha * slice_end)
-            ) / (
-                np.exp(-self.population_inversion.crystal_alpha * z)
-                * self.length
-                * self.population_inversion.crystal_alpha
-            )
 
             # integrate super-gaussian
             g_order = self.population_inversion.pump_gaussian_order
@@ -422,17 +410,19 @@ class CrystalSlice(Element):
             seed_wavelength = 800.0  # [nm]
             fraction_to_heating = (seed_wavelength - pump_wavelength) / seed_wavelength
 
+            dz = self.length
+
             energy_term = (
                 (self.population_inversion.pump_wavelength / (const.h * const.c))
                 * (1.0 - fraction_to_heating)
                 * self.population_inversion.pump_energy
             )
+
             alpha_term = (
-                1
-                - np.exp(
-                    -self.population_inversion.crystal_alpha * self.length * nslice
-                )
-            ) * np.exp(-self.population_inversion.crystal_alpha * z)
+                np.exp(-self.population_inversion.crystal_alpha * slice_front)
+                - np.exp(-self.population_inversion.crystal_alpha * slice_end)
+            ) / (self.population_inversion.crystal_alpha * dz)
+
             radial_term = np.exp(
                 -2.0
                 * (
@@ -444,21 +434,17 @@ class CrystalSlice(Element):
                 )
                 ** g_order
             )
-            dz = self.length * nslice
 
             # Create mesh of [num_excited_states/m^3] pop_inversion_mesh
             temp_mesh = (
-                energy_term
-                * alpha_term
-                * radial_term
-                * correction_factor
-                * integral_factor
-                / dz
+                energy_term * alpha_term * radial_term * integral_factor / (dz * nslice)
             )
 
-            pop_inversion_mesh += temp_mesh.astype("float64")
+            excited_states += temp_mesh.astype("float64")
 
-        self.pop_inversion_mesh = pop_inversion_mesh
+        self.pop_inversion_mesh = (
+            2.0 * excited_states
+        )  # population inversion = N2 - N1 = 2* (number of excited states)
 
     def _propagate_attenuate(self, laser_pulse, calc_gain, nl_kick):
         # n_x = wfront.mesh.nx  #  nr of grid points in x
@@ -844,8 +830,8 @@ class CrystalSlice(Element):
             temp_array = np.copy(self.pop_inversion_mesh)
 
             a_x = np.linspace(
-                -self.population_inversion.mesh_extent,
-                self.population_inversion.mesh_extent,
+                -self.population_inversion.mesh_extent * 1.15,
+                self.population_inversion.mesh_extent * 1.15,
                 self.population_inversion.n_cells,
             )
             a_y = a_x
@@ -859,8 +845,8 @@ class CrystalSlice(Element):
             a_x = a.x
             a_y = a.y
             b_x = np.linspace(
-                -self.population_inversion.mesh_extent,
-                self.population_inversion.mesh_extent,
+                -self.population_inversion.mesh_extent * 1.15,
+                self.population_inversion.mesh_extent * 1.15,
                 self.population_inversion.n_cells,
             )
             b_y = b_x
@@ -877,11 +863,14 @@ class CrystalSlice(Element):
             dx = (
                 2.0
                 * self.population_inversion.mesh_extent
+                * 1.15
                 / self.population_inversion.n_cells
             )
             b_xv, b_yv = np.meshgrid(b_x, b_y)
             b_r = np.sqrt(b_xv**2.0 + b_yv**2.0)
-            temp_array[b_r > self.population_inversion.mesh_extent - 0.9 * dx] = 0.0
+            temp_array[
+                b_r > self.population_inversion.mesh_extent * 1.15 - 0.9 * dx
+            ] = 0.0
 
         return temp_array
 